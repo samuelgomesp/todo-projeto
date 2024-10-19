54942ad3000d0e38647d20db60c872b76d1ba9a9
Samuel Gomes <samuel.gomes.p99@gmail.com>
2024-10-19 18:04:22 -0300

    Commit inicial para envio para o GitHub

---
+++ js/script.js
@@ -0,0 +1,165 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const srcInput = document.querySelector('#search-input')
+const srcForm = document.querySelector('#search-form')
+const eraseButton = document.querySelector('#erase-button')
+const selectForm = document.querySelector('#filter-select')
+
+let oldInputValue;
+
+// Funções
+
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+    todoInput.value = "";
+    todoInput.focus();
+
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        };
+    });
+};
+
+// Eventos
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+
+    if (inputValue) {
+        saveTodo(inputValue)
+    };
+});
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    };
+
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
+    };
+
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    };
+
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms();
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    };
+
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const editInputValue = editInput.value
+
+    if(editInputValue){
+        updateTodo(editInputValue);
+    };
+
+    toggleForms();
+});
+
+eraseButton.addEventListener('click', function(e){
+    e.preventDefault()
+    srcInput.value = ''
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        todo.style.display = "flex"
+    });
+
+})
+
+srcInput.addEventListener('input', (e) => {
+    let inputed = e.target.value.toLowerCase()
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        let text = todo.textContent.toLowerCase()
+        if (text.includes(inputed)) {
+        todo.style.display = "flex"
+        } else {
+        todo.style.display = "none"
+        }
+    });
+})
+
+selectForm.addEventListener('click', (e) => {
+    let selectValue =  e.target.value
+    const todos = document.querySelectorAll(".todo")
+    todos.forEach((todo) => {
+        if(selectValue === 'all'){
+            todo.style.display = 'flex'
+        } 
+        else if(selectValue === 'done' && todo.className === 'todo done'){
+            todo.style.display = 'flex'
+        }
+        else if(selectValue === 'todo' && todo.className === 'todo'){
+            todo.style.display = 'flex'
+        } else {
+            todo.style.display = 'none'
+        }
+    })
+
+})
